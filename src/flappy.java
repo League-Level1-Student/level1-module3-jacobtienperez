9b1f2c91bbf8ba06c3b7db15976726268b11094b
perez.jacob <perez.jacob@SEMS-259-30.SEMS259.LAB>
2018-12-12 16:29:30 -0800

    flappy

---
+++ src/flappy.java
@@ -0,0 +1,40 @@
+
+public class flappy {
+	/*float birdx = 250;
+	  float birdy = 401;
+	  float birdv = 1 ;
+	  float birdg = .1;
+	  float flap = .45;
+	  float pipex = 300;
+	  float pipespeed = 10;
+	  float upperpipeheight = (int) random(100, 400);
+	void setup(){
+	  
+	  
+	size(700, 500 );
+
+	}
+	void draw(){
+	  background((255),(85),57); 
+	fill(69,21,28);
+	if(mousePressed){
+	  birdv = birdv-flap;
+	}
+
+	birdv= birdv + birdg;
+	birdy = birdy+birdv;
+	ellipse(250,birdy,50,50);
+	rect(pipex,1,50,upperpipeheight);
+	pipex= pipex-pipespeed;
+	teleportpipe();} 
+
+
+	void teleportpipe(){
+	  if(pipex<0){
+	    pipex=700;
+	  }
+	    
+	    
+	}
+	*/
+}
